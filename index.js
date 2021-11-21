dd013bf729c9329191b4481a155ee9ef4c64fbd0
Rodgers31 <otienor986@gmail.com>
2021-11-21 17:39:09 -0500

    ready to deploy

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const server = require('express')();
+const express = require('express');
+const cors = require('cors');
+const server = express();
 
-const PORT = 5000;
+server.use(express.json());
+server.use(cors());
+const PORT = process.env.PORT || 5000;
+
+server.get('/', (req, res) => {
+  res.send(`<h1>Running app</>`);
+});
+
+server.get('/api', (req, res) => {
+  res.json({ message: 'more testing' });
+});
+
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
