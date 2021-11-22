0351f201c5dd2b94ed24cd258ed33c1fff1e111d
Rodgers31 <otienor986@gmail.com>
2021-11-21 19:03:19 -0500

    added react app

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 require('dotenv').config(); //this allows us to stash "artificial env variables" in a file
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const server = express();
 
-server.use(express.json());
-server.use(cors());
 const PORT = process.env.PORT || 5000;
 
-server.get('/', (req, res) => {
-  res.send(`<h1>Running app</>`);
-});
+server.use(express.json());
+server.use(cors());
+server.use(express.static(path.join(__dirname, 'client_side/build')));
 
 server.get('/api', (req, res) => {
   res.json({ message: 'more testing' });
+});
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client_side/build', 'index.html'));
 });
 
 server.listen(PORT, () => {
